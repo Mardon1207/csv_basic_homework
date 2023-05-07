2351b2a8eeef90fab06abf8351ed48441f35e853
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-07 22:40:52 +0500

    5

--- get_first_row.py
+++ get_first_row.py
@@ -1,12 +1,17 @@
-def get_first_row(data):   
-   """
-   Get the first row from a CSV file.
-    Args:
-        data(str): csv file.
-    Return:
-        list: First row.
-   """
-   
-   return
-
+from csv import reader
+#Define function,Get coloumn names from a csv file
+def get_column_names(data):   
+    """ 
+    Get column names from a csv file
+    Parameters:
+        data(str): csv file
+    Returns:
+        column_names: list of column names
+    """
+    f=open('data.csv')
+    r=reader(f,delimiter=',')
+    return list(r)[1]
+print(get_column_names('data.csv'))
+    
+    
 # Read the csv file
